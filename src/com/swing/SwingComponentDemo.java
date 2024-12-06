f7fb13ee26076dc68a9bcd82301fef0567ef3f2d
zitcen <woshizitcen@163.com>
2024-12-06 16:40:43 +0800

    swing

--- src/com/swing/SwingComponentDemo.java
+++ src/com/swing/SwingComponentDemo.java
@@ -1,6 +1,7 @@
 package com.swing;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * @ClassName SwingComponentDemo
@@ -27,8 +28,58 @@ public class SwingComponentDemo {
     private JMenuItem annotationMenu = new JMenuItem("注释");
     private JMenuItem delAnnotationMenu = new JMenuItem("取消注释");
     //添加具体内容
+    //定义上部分的案例
+    //定义一个8行20列的多行文本域
+    JTextArea ta = new JTextArea(8,20);
+    //添加下拉框
+    //定义一个数组存储颜色
+    String[] colors = { "红色", "绿色 " , "蓝色 " };
+    JComboBox<String> colorChooser = new JComboBox<String>(colors);
+    //添加单选框
+    JRadioButton female = new JRadioButton("女");
+    JRadioButton male = new JRadioButton("男");
+    //单选框组
+    ButtonGroup bg = new ButtonGroup();
+    //复选框
+    JCheckBox married = new JCheckBox("是否已婚");
+    //添加list
+    JList colorList = new JList(colors);
+
+    //底部按钮和文本框
+    JTextField name = new JTextField(40);
+//    Icon okIcon = new ImageIcon(ImagePathUtil.getRealPath("2\\ok.png"));
+    JButton ok = new JButton("确定");
+
+    //
 
     private void init(){
+
+        Box leftBox = Box.createVerticalBox();
+        JPanel boxPanel = new JPanel();
+        boxPanel.add(colorChooser);
+        bg.add(male);
+        bg.add(female);
+        boxPanel.add(male);
+        boxPanel.add(female);
+        boxPanel.add(married);
+        leftBox.add(ta);
+        leftBox.add(boxPanel);
+//        jf.add(leftBox);
+
+
+        Box topBox = Box.createHorizontalBox();
+        topBox.add(leftBox);
+        topBox.add(Box.createHorizontalGlue());
+        topBox.add(colorList);
+        jf.add(topBox);
+
+
+        //创建文本框和按钮
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(name);
+        buttonPanel.add(ok);
+        jf.add(buttonPanel,BorderLayout.SOUTH);
+
         //添加菜单组件
         jf.setJMenuBar(jmb);
         encodeMenu.add(annotationMenu);
